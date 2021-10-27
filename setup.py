7f2da9f8f17ec583507deb1ca44bb91019508e42
cetteup <me@cetteup.com>
2021-10-27 13:24:28 +0200

    Bump version to 0.1.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='pybfbc2stats',
-    version='0.1.1',
+    version='0.1.2',
     description='Simple Python library for retrieving statistics of Battlefield: Bad Company 2 players',
     long_description=long_description,
     long_description_content_type='text/markdown',
