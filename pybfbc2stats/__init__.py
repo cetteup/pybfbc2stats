241c6a99e0185f734edc70e14b5f839e672e0902
cetteup <me@cetteup.com>
2021-10-27 12:16:13 +0200

    Expose exceptions in __init__

--- pybfbc2stats/__init__.py
+++ pybfbc2stats/__init__.py
@@ -1,5 +1,7 @@
 from .connection import Connection
 from .client import Client
+from .exceptions import PyBfbc2StatsError, PyBfbc2StatsTimeoutError, PyBfbc2StatsParameterError, \
+    PyBfbc2StatsNotFoundError
 
 """
 pybfbc2stats.
@@ -9,4 +11,5 @@ Simple Python library for retrieving statistics of Battlefield: Bad Company 2 pl
 __version__ = '0.1.0'
 __author__ = 'cetteup'
 __credits__ = 'nemo, Luigi Auriemma'
-__all__ = ['Connection', 'Client']
+__all__ = ['Connection', 'Client', 'PyBfbc2StatsError', 'PyBfbc2StatsParameterError', 'PyBfbc2StatsTimeoutError',
+           'PyBfbc2StatsNotFoundError']
