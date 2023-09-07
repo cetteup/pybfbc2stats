ca8738a75a2d2af2b0bd39c490d42eaa4f0521b4
cetteup <me@cetteup.com>
2023-09-07 16:13:39 +0200

    feat: Split stats keys into categories

--- pybfbc2stats/__init__.py
+++ pybfbc2stats/__init__.py
@@ -2,7 +2,8 @@ from .asyncio_client import AsyncFeslClient, AsyncTheaterClient
 from .asyncio_connection import AsyncConnection, AsyncSecureConnection
 from .client import FeslClient, TheaterClient
 from .connection import Connection, SecureConnection
-from .constants import Platform, Namespace, DEFAULT_LEADERBOARD_KEYS, STATS_KEYS
+from .constants import Platform, Namespace, DEFAULT_LEADERBOARD_KEYS, STATS_KEYS, GENERAL_STATS_KEYS, WEAPON_STATS_KEYS, \
+    VEHICLE_STATS_KEYS, SERVICE_STARS_STATS_KEYS, AWARDS_STATS_KEYS
 from .exceptions import Error, TimeoutError, ParameterError, \
     PlayerNotFoundError, AuthError, ConnectionError, SearchError, \
     NotFoundError, ServerNotFoundError, LobbyNotFoundError, RecordNotFoundError
@@ -17,7 +18,8 @@ __author__ = 'cetteup'
 __credits__ = 'nemo, Luigi Auriemma'
 __all__ = ['Connection', 'SecureConnection', 'FeslClient', 'TheaterClient',
            'AsyncConnection', 'AsyncSecureConnection', 'AsyncFeslClient', 'AsyncTheaterClient',
-           'Platform', 'Namespace', 'DEFAULT_LEADERBOARD_KEYS', 'STATS_KEYS',
+           'Platform', 'Namespace', 'DEFAULT_LEADERBOARD_KEYS', 'STATS_KEYS', 'GENERAL_STATS_KEYS', 'WEAPON_STATS_KEYS',
+           'VEHICLE_STATS_KEYS', 'SERVICE_STARS_STATS_KEYS', 'AWARDS_STATS_KEYS',
            'Error', 'ConnectionError', 'ParameterError', 'TimeoutError',
            'AuthError', 'NotFoundError', 'PlayerNotFoundError', 'RecordNotFoundError',
            'ServerNotFoundError', 'LobbyNotFoundError', 'SearchError']
