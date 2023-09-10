d7a828f282f77af0129b742195d94db93172c2ac
cetteup <me@cetteup.com>
2023-09-10 21:15:09 +0200

    fix: Export singleplayer stats keys from package root

--- pybfbc2stats/__init__.py
+++ pybfbc2stats/__init__.py
@@ -3,7 +3,7 @@ from .asyncio_connection import AsyncConnection, AsyncSecureConnection
 from .client import FeslClient, TheaterClient
 from .connection import Connection, SecureConnection
 from .constants import Platform, Namespace, DEFAULT_LEADERBOARD_KEYS, STATS_KEYS, GENERAL_STATS_KEYS, WEAPON_STATS_KEYS, \
-    VEHICLE_STATS_KEYS, SERVICE_STARS_STATS_KEYS, AWARDS_STATS_KEYS
+    VEHICLE_STATS_KEYS, SERVICE_STARS_STATS_KEYS, AWARDS_STATS_KEYS, SINGLEPLAYER_STATS_KEYS
 from .exceptions import Error, TimeoutError, ParameterError, \
     PlayerNotFoundError, AuthError, ConnectionError, SearchError, \
     NotFoundError, ServerNotFoundError, LobbyNotFoundError, RecordNotFoundError
@@ -19,7 +19,7 @@ __credits__ = 'nemo, Luigi Auriemma'
 __all__ = ['Connection', 'SecureConnection', 'FeslClient', 'TheaterClient',
            'AsyncConnection', 'AsyncSecureConnection', 'AsyncFeslClient', 'AsyncTheaterClient',
            'Platform', 'Namespace', 'DEFAULT_LEADERBOARD_KEYS', 'STATS_KEYS', 'GENERAL_STATS_KEYS', 'WEAPON_STATS_KEYS',
-           'VEHICLE_STATS_KEYS', 'SERVICE_STARS_STATS_KEYS', 'AWARDS_STATS_KEYS',
+           'VEHICLE_STATS_KEYS', 'SERVICE_STARS_STATS_KEYS', 'AWARDS_STATS_KEYS', 'SINGLEPLAYER_STATS_KEYS',
            'Error', 'ConnectionError', 'ParameterError', 'TimeoutError',
            'AuthError', 'NotFoundError', 'PlayerNotFoundError', 'RecordNotFoundError',
            'ServerNotFoundError', 'LobbyNotFoundError', 'SearchError']
