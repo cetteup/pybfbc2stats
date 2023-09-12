9c2c61ea1a6541c7bb529bc78ed7582769f07ae6
cetteup <me@cetteup.com>
2023-09-12 07:57:55 +0200

    tag: Bump version to 0.5.2

--- pybfbc2stats/__init__.py
+++ pybfbc2stats/__init__.py
@@ -13,7 +13,7 @@ pybfbc2stats.
 Python library for retrieving statistics of Battlefield: Bad Company 2 players.
 """
 
-__version__ = '0.5.1'
+__version__ = '0.5.2'
 __author__ = 'cetteup'
 __credits__ = 'nemo, Luigi Auriemma'
 __all__ = ['Connection', 'SecureConnection', 'FeslClient', 'TheaterClient',
